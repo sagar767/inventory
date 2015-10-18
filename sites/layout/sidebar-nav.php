83f5f4b96138558d58a4d9859954adfe322bdc5d
Tamaghna Banerjee <tamaghna@novatree.com>
2015-10-19 00:33:00 +0530

    Home Page layout created

---
+++ sites/layout/sidebar-nav.php
@@ -0,0 +1,17 @@
+<div class="col-sm-3">
+      <!-- left -->
+      <h3><i class="glyphicon glyphicon-briefcase"></i> Toolbox</h3>
+      <hr>
+      
+      <ul class="nav nav-stacked">
+        <li><a href="javascript:;"><i class="glyphicon glyphicon-flash"></i> Alerts</a></li>
+        <li><a href="javascript:;"><i class="glyphicon glyphicon-link"></i> Links</a></li>
+        <li><a href="javascript:;"><i class="glyphicon glyphicon-list-alt"></i> Reports</a></li>
+        <li><a href="javascript:;"><i class="glyphicon glyphicon-book"></i> Books</a></li>
+        <li><a href="javascript:;"><i class="glyphicon glyphicon-briefcase"></i> Tools</a></li>
+        <li><a href="javascript:;"><i class="glyphicon glyphicon-time"></i> Real-time</a></li>
+        <li><a href="javascript:;"><i class="glyphicon glyphicon-plus"></i> Advanced..</a></li>
+      </ul>
+      
+      <hr>
+ </div>
---
+++ sites/layout/sub-nav.php
@@ -0,0 +1,40 @@
+<div class="col-md-5">
+              
+                <ul class="nav nav-justified">
+         			<li><a href="#"><i class="glyphicon glyphicon-cog"></i></a></li>
+         			<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-comment"></i><span class="count">3</span></a><ul class="dropdown-menu" role="menu"><li><a href="#">1. Is there a way..</a></li><li><a href="#">2. Hello, admin. I would..</a></li><li><a href="#"><strong>All messages</strong></a></li></ul></li>
+         			<li><a href="#"><i class="glyphicon glyphicon-user"></i></a></li>
+         			<li><a title="Add Widget" data-toggle="modal" href="#addWidgetModal"><span class="glyphicon glyphicon-plus-sign"></span></a></li>
+       			</ul>  
+              
+                <hr>
+              
+				<p>
+                  This is a responsive dashboard-style layout that uses <a href="http://www.getbootstrap.com">Bootstrap 3</a>. You can use this template as a starting point to create something more unique.
+                </p>
+                <p>
+                  Visit the Bootstrap Playground at <a href="http://www.bootply.com">Bootply</a> to tweak this layout, or discover 1000's of Bootstrap code examples and snippets.
+                </p>
+              
+                <hr>
+              
+                <div class="btn-group btn-group-justified">
+                  <a href="#" class="btn btn-info col-sm-3">
+                    <i class="glyphicon glyphicon-plus"></i><br>
+                    Product
+                  </a>
+                  <a href="#" class="btn btn-info col-sm-3">
+                    <i class="glyphicon glyphicon-cloud"></i><br>
+                    Cloud
+                  </a>
+                  <a href="#" class="btn btn-info col-sm-3">
+                    <i class="glyphicon glyphicon-cog"></i><br>
+                    Tools
+                  </a>
+                  <a href="#" class="btn btn-info col-sm-3">
+                    <i class="glyphicon glyphicon-question-sign"></i><br>
+                    Help
+                  </a>
+                </div>
+              
+			</div><!--/col-span-6-->
